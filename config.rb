49928d822115dd637e6c69bf50108e1e24ffe53e
Daniel Fuller <lasaldan@gmail.com.com>
2015-06-18 07:39:59 -0600

    Also fixed a bug in ionic-ios - Date() workaround

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 preferred_syntax = :scss
 http_path = '/'
 css_dir = 'www/css/'
-sass_dir = 'www/lib/ionic/scss'
+sass_dir = 'scss'
 images_dir = 'www/img'
 javascripts_dir = 'www/js'
 relative_assets = true
